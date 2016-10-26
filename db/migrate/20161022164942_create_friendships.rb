e60c52094b4765d83513eefb6e1ed217c256c2ca
ob00042 <ob00042@gmail.com>
2016-10-26 16:39:32 +0300

    Add fix in db

--- db/migrate/20161022164942_create_friendships.rb
+++ db/migrate/20161022164942_create_friendships.rb
@@ -1,7 +1,5 @@
 class CreateFriendships < ActiveRecord::Migration[5.0]
   def change
-  	drop_table :friendships
-  	
     create_table :friendships do |t|
       t.references :user, foreign_key: true
       t.integer :friend_id, foreign_key: true
